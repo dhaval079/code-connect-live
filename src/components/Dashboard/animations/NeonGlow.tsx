b68ef9864f93e72d608f4787e0f165ebcba556b8
dhaval079 <rupaparadhaval1234@gmail.com>
2025-06-17 20:53:10 +0530

    Update NeonGlow

--- src/components/Dashboard/animations/NeonGlow.tsx
+++ src/components/Dashboard/animations/NeonGlow.tsx
@@ -19,7 +19,7 @@ export const NeonGlow = () => {
         transition={{ duration: 2 }}
       >
         <div className="absolute top-0 left-0 w-1/2 h-1/2 bg-blue-500 rounded-full filter blur-[150px] opacity-20" />
-        <div className="absolute bottom-0 right-0 w-1/2 h-1/2 bg-purple-500 rounded-full filter blur-[150px] opacity-20" />
+        <div className="absolute bottom-0 right-0 w-1/2 h-1/2 bg-purple-500 rounded-full filter blur-[150px] opacity-30" />
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 bg-cyan-500 rounded-full filter blur-[150px] opacity-20" />
       </motion.div>
     )
